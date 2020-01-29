04f608a0c697ad200471f319aed14faccd741ad1
YangYu8595 <60034617+YangYu8595@users.noreply.github.com>
2020-01-29 22:21:06 +0100

    Update moveRabbit.js

--- work/report/js/moveRabbit.js
+++ work/report/js/moveRabbit.js
@@ -2,8 +2,6 @@
   function move(){
     rabbit.style.left = rabbit.style.left + 20 +'px';
   }
-  rabbit.style.left = '0px';
-  rabbit.style.top = '0px';
   rabbit.addEventListener('click',move);
   /*rabbit.onclick = function(){
     rabbit.style.left = rabbit.style.left + 20;
