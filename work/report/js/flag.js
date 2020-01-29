3a6e63833bac0d788fd3095cb48b817fc7b1a28c
YangYu8595 <60034617+YangYu8595@users.noreply.github.com>
2020-01-29 22:58:55 +0100

    Update flag.js

--- work/report/js/flag.js
+++ work/report/js/flag.js
@@ -3,7 +3,9 @@
 
     //var myContent = document.getElementById('content');
     var flagTarget = document.getElementById('flag');
-    var flagLink = document.getElementById('draw-elfenbenskusten');
+    var flagLink1 = document.getElementById('draw-elfenbenskusten');
+    var flagLink2 = document.getElementById('draw-france');
+    var flagLink3 = document.getElementById('draw-italy');
 
     //myContent.innerHTML = '<h3>This is a MEGA template!</h3>';
 
@@ -14,13 +16,32 @@
         console.log("Drawing flag");
         flagTarget.innerHTML = flagElfenbenskusten;
     }
+    function drawFlagFrance()  {
+        var flagFrance = '<div class="flag france"><div class="part1"></div>' +
+        '<div class="part2"></div></div>';
 
+        console.log("Drawing flag");
+        flagTarget.innerHTML = flagFrance;
+    }
+    function drawFlagItaly()  {
+        var flagItaly = '<div class="flag italy"><div class="part1"></div>' +
+        '<div class="part2"></div></div>';
 
-    flagLink.addEventListener("click", function () {
+        console.log("Drawing flag");
+        flagTarget.innerHTML = flagItaly;
+    }
+    flagLink1.addEventListener("click", function () {
         console.log("Event for clicking link elfenbenskusten.");
         drawFlagElfenbenskusten();
     });
-
+    flagLink2.addEventListener("click", function () {
+        console.log("Event for clicking link france.");
+        drawFlagFrance();
+    });
+    flagLink3.addEventListener("click", function () {
+        console.log("Event for clicking link itlay.");
+        drawFlagItaly();
+    });
 
     console.log('Flag Page is ready!');
 })();
