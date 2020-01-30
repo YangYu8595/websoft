af838c5966c464581ad468e085045fb09f864669
YangYu8595 <60034617+YangYu8595@users.noreply.github.com>
2020-01-30 18:11:15 +0100

    Update moveRabbit.js

--- work/report/js/moveRabbit.js
+++ work/report/js/moveRabbit.js
@@ -2,6 +2,6 @@
 ( function (){
   var r = document.getElementById("rabbit");
   r.addEventListener("click",function(){
-    r.style.left = r.offsetLeft + 40 "px";
+    r.style.left = r.offsetLeft + 40 + "px";
   }
 });
