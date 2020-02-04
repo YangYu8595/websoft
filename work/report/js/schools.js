de5b374ae0bc66975807922e6bf8f0cfa362592e
YangYu8595 <yy85959948@gmail.com>
2020-02-04 22:21:32 +0100

    lab3

--- work/report/js/schools.js
+++ work/report/js/schools.js
@@ -2,7 +2,8 @@
     'use strict';
 
     let url;
-
+    var btn = document.getElementById("button");
+    var schools;
     //url = "https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1081";
     url = "data/1081.json";
     fetch(url)
@@ -10,12 +11,16 @@
             return response.json();
         })
         .then((myJson) => {
-	    var data = document.getElementById('dataBox');
-	    data.innerHTML = JSON.stringify(myJson,null,4);
+	    //var data = document.getElementById('dataBox');
+	    //data.innerHTML = JSON.stringify(myJson,null,4);
 		//$("#dataBox").text(JSON.stringify(myJson, null, 4));
             console.log(myJson);
+	    schools = JOSN.stringify(myJson,null,4);
         });
-
+	btn.onclick = function(){  
+        var data = document.getElementById('dataBox');
+        data.innerHTML = schools;
+ 	}  
    /* url = "https://rem.dbwebb.se/api/users";
     fetch(url)
         .then((response) => {
