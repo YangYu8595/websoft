9d77538550215a7f23de8201c6f4e2d23df0b2d7
YangYu8595 <yy85959948@gmail.com>
2020-02-23 10:19:36 +0100

    lab3

--- work/report/js/schools.js
+++ work/report/js/schools.js
@@ -4,7 +4,7 @@
     let url;
     var btn = document.getElementById("button");
     var schools;
-    var datas;
+
     //url = "https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1081";
     url = "data/1081.json";
     fetch(url)
@@ -25,7 +25,16 @@
 	    var table = document.getElementById("table");
 	    //for (var i = 0; i < datas.length; i ++){
 		var row = table.insertRow(table.rows.length);
-		//var row = table.insertRow(table.rows.length);
+		var C1 = row.insertCell(0);
+                C1.innerHTML = "Skolenhetskod";
+                var c2=row.insertCell(1);
+                c2.innerHTML="Skolenhetsnamn";
+                var c3=row.insertCell(2);
+                c3.innerHTML="Kommunkod";
+                var c4 = row.insertCell(3);
+                c4.innerHTML = "PeOrgNr";
+
+		var row = table.insertRow(table.rows.length);
 		var C1 = row.insertCell(0);
 		C1.innerHTML = myJson[0].Skolenhetskod; 
  		var c2=row.insertCell(1); 
@@ -38,12 +47,12 @@
             
  	}  
 
-	    schools = myJson;
+	    /*schools = myJson;
 	    myJson.Skolenheter = myJson.Skolenheter.filter(function(a){
 		return a.Skolenhetsnamn ==="Komvux";
 	    });
 	    datas = myJson.Skolenheter;
-	    
+	    */
 
         });
 	/*btn.onclick = function(){  
@@ -68,5 +77,5 @@
             data.innerHTML = JSON.stringify(schools.Skolenheter,0,4);
  	}  */
 
-    console.log('Sandbox is ready!');
+    console.log('Data is ready!');
 })();
