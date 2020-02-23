3fa74f803b3263735086b6f4fb489d2ef9c9db4e
YangYu8595 <60034617+YangYu8595@users.noreply.github.com>
2020-02-23 08:22:09 +0100

    Update schools.js

--- work/report/js/schools.js
+++ work/report/js/schools.js
@@ -16,6 +16,28 @@
 	    //data.innerHTML = JSON.stringify(myJson,null,4);
 		//$("#dataBox").text(JSON.stringify(myJson, null, 4));
             console.log(myJson);
+	    btn.onclick = function(){  
+            var data = document.getElementById('dataBox');
+	    schools.Skolenheter = schools.Skolenheter.filter(function (a){
+		//if (a.Skolenhetsnamn == "Komvux"){
+	        return a.Skolenhetsnamn ==="Komvux";
+	    });
+	    var table = document.getElementById("table");
+	    //for (var i = 0; i < datas.length; i ++){
+		var row = table.insertRow(table.rows.length);
+		//var row = table.insertRow(table.rows.length);
+		var C1 = row.insertCell(0);
+		C1.innerHTML = myJson[0].Skolenhetskod; 
+ 		var c2=row.insertCell(1); 
+ 		c2.innerHTML=myJson[0].Skolenhetsnamn; 
+ 		var c3=row.insertCell(2); 
+ 		c3.innerHTML=myJson[0].Kommunkod;
+		var c4 = row.insertCell(3);
+		c4.innerHTML = myJson[0].PeOrgNr;
+	    //}
+            
+ 	}  
+
 	    schools = myJson;
 	    myJson.Skolenheter = myJson.Skolenheter.filter(function(a){
 		return a.Skolenhetsnamn ==="Komvux";
@@ -24,7 +46,7 @@
 	    
 
         });
-	btn.onclick = function(){  
+	/*btn.onclick = function(){  
             var data = document.getElementById('dataBox');
 	    schools.Skolenheter = schools.Skolenheter.filter(function (a){
 		//if (a.Skolenhetsnamn == "Komvux"){
@@ -44,7 +66,7 @@
 		c4.innerHTML = schools[0].PeOrgNr;
 	    //}
             data.innerHTML = JSON.stringify(schools.Skolenheter,0,4);
- 	}  
+ 	}  */
 
     console.log('Sandbox is ready!');
 })();
