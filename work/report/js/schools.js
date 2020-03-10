239ba7b572549d9e093eac1c658269182835a3e1
YangYu8595 <yy85959948@gmail.com>
2020-03-10 10:40:01 +0100

    lab3

--- work/report/js/schools.js
+++ work/report/js/schools.js
@@ -23,7 +23,7 @@
 	                  return a.Skolenhetsnamn ==="Komvux";
                  }
 	           });
-             
+             delete myJson.Fotnot;
              console.log(myJson);
 	    var table = document.getElementById("table");
 	    //for (var i = 0; i < datas.length; i ++){
