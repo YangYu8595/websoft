287e74d4aaf087070e37d22b458114ea40cf55fc
YangYu8595 <yy85959948@gmail.com>
2020-03-10 09:59:21 +0100

    lab3

--- work/report/js/schools.js
+++ work/report/js/schools.js
@@ -5,8 +5,8 @@
     var btn = document.getElementById("button");
     var schools;
 
-    //url = "https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1081";
-    url = "data/1081.json";
+    url = "https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1081";
+    //url = "data/1081.json";
     fetch(url)
         .then((response) => {
             return response.json();
@@ -16,12 +16,12 @@
 	    //data.innerHTML = JSON.stringify(myJson,null,4);
 		//$("#dataBox").text(JSON.stringify(myJson, null, 4));
             console.log(myJson);
-	    btn.onclick = function(){  
+	    btn.onclick = function(){
             var data = document.getElementById('dataBox');
-	    /*schools.Skolenheter = schools.Skolenheter.filter(function (a){
-		//if (a.Skolenhetsnamn == "Komvux"){
+	    myJson.Skolenheter = myJson.Skolenheter.filter(function (a){
+		      if (a.Skolenhetsnamn == "Komvux"){
 	        return a.Skolenhetsnamn ==="Komvux";
-	    });*/
+	    });
 	    var table = document.getElementById("table");
 	    //for (var i = 0; i < datas.length; i ++){
 		var row = table.insertRow(table.rows.length);
@@ -36,16 +36,16 @@
 
 		var row = table.insertRow(table.rows.length);
 		var C1 = row.insertCell(0);
-		C1.innerHTML = myJson[0].Skolenhetskod; 
- 		var c2=row.insertCell(1); 
- 		c2.innerHTML=myJson[0].Skolenhetsnamn; 
- 		var c3=row.insertCell(2); 
+		C1.innerHTML = myJson[0].Skolenhetskod;
+ 		var c2=row.insertCell(1);
+ 		c2.innerHTML=myJson[0].Skolenhetsnamn;
+ 		var c3=row.insertCell(2);
  		c3.innerHTML=myJson[0].Kommunkod;
 		var c4 = row.insertCell(3);
 		c4.innerHTML = myJson[0].PeOrgNr;
 	    //}
-            
- 	}  
+
+ 	}
 
 	    /*schools = myJson;
 	    myJson.Skolenheter = myJson.Skolenheter.filter(function(a){
@@ -55,7 +55,7 @@
 	    */
 
         });
-	/*btn.onclick = function(){  
+	/*btn.onclick = function(){
             var data = document.getElementById('dataBox');
 	    schools.Skolenheter = schools.Skolenheter.filter(function (a){
 		//if (a.Skolenhetsnamn == "Komvux"){
@@ -66,10 +66,10 @@
 		var row = table.insertRow(table.rows.length);
 		//var row = table.insertRow(table.rows.length);
 		var C1 = row.insertCell(0);
-		C1.innerHTML = schools[0].Skolenhetskod; 
- 		var c2=row.insertCell(1); 
- 		c2.innerHTML=schools[0].Skolenhetsnamn; 
- 		var c3=row.insertCell(2); 
+		C1.innerHTML = schools[0].Skolenhetskod;
+ 		var c2=row.insertCell(1);
+ 		c2.innerHTML=schools[0].Skolenhetsnamn;
+ 		var c3=row.insertCell(2);
  		c3.innerHTML=schools[0].Kommunkod;
 		var c4 = row.insertCell(3);
 		c4.innerHTML = schools[0].PeOrgNr;
