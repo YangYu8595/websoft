bad8f304ee66472547f296c4ed0880ed3b7ca495
YangYu8595 <yy85959948@gmail.com>
2020-03-13 15:40:40 +0100

    lab3

--- work/report/js/moveRabbit.js
+++ work/report/js/moveRabbit.js
@@ -1,13 +1,22 @@
 "use strict";
 ( function (){
   var r = document.getElementById("rabbit");
+  var b = document.getElementById("body");
   r.addEventListener("click",function(){
-    r.style.left = r.offsetLeft + 40 + "px";
-    console.log(r.style.left);
+    if(get_xPosition(r)+r.offsetWidth+40 < b.offsetWidth){
+      r.style.left = r.offsetLeft + 40 + "px";
+      console.log(r.style.left);
+    }
+
   });
   var timer = 5000;
   function disappear(){
-    r.style.visibility="hidden";
+    if(r.style.visibility==="visible"){
+      r.style.visibility="hidden";
+    }
+    else{
+      r.style.visibility="visible";
+    }
   }
   window.setInterval(disappear,timer);
 })();
