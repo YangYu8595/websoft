3795aa77cd2e9cfaf11f0ac4ae23d4e641762053
YangYu8595 <yy85959948@gmail.com>
2020-03-14 13:32:32 +0100

    lab4

--- work/s04/a04/route/lotto.js
+++ work/s04/a04/route/lotto.js
@@ -4,15 +4,40 @@ var express = require('express');
 var router  = express.Router();
 
 var arr = [];
-for (var i = 0; i < 7; i++){
-   arr[i] = Math.floor(Math.random()*35+1);
-}
+var reply = {};
+var data;
+var strings;
+var arrs = [];
+var url;
+
 
 // Add a route for the path /
 router.get('/lotto', (req, res) => {
-   res.send(arr);
+  for (var i = 0; i < 7; i++){
+     arr[i] = Math.floor(Math.random()*35+1);
+  }
+  reply.drwanNumbers = arr;
+  url= req.url;
+  var matchs = 0;
+  if(url.indexOf("?") != -1){
+     strings = url.split("?")[1];
+     data = strings.split("=")[1];
+     arrs = data.split(",");
+     for(var i = 0; i < arrs.length; i++){
+       for(var j = 0; j < arr.length; j++){
+         if(arr[j]==arrs[i]){
+           matchs++;
+         }
+       }
+     }
+     reply.ownNumbers = arrs;
+     reply.correctNumbers = matchs;
+  }
+
+
+   res.render("lotto",reply);
     //res.send(Math.floor(Math.random()*35+1));
 
 });
 
-module.exports = router; 
+module.exports = router;
