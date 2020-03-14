45503dec2d8501e907e4d05c4eedafd87f8b7ea4
YangYu8595 <yy85959948@gmail.com>
2020-03-14 11:51:02 +0100

    lab3

--- work/report/js/moveRabbit.js
+++ work/report/js/moveRabbit.js
@@ -3,10 +3,10 @@
   var r = document.getElementById("rabbit");
   var b = document.getElementById("body");
   r.addEventListener("click",function(){
-    if(r.style.left+r.offsetWidth+40 < b.offsetWidth){
+    //if(r.style.left+r.offsetWidth+40 < b.offsetWidth){
       r.style.left = r.offsetLeft + 40 + "px";
       console.log(r.style.left);
-    }
+    //}
 
   });
   var timer = 5000;
