b5e6e289b9a0bc1698c9e75b46921ee2462c7ab2
YangYu8595 <yy85959948@gmail.com>
2020-03-14 11:45:16 +0100

    lab3

--- work/report/js/moveRabbit.js
+++ work/report/js/moveRabbit.js
@@ -3,7 +3,7 @@
   var r = document.getElementById("rabbit");
   var b = document.getElementById("body");
   r.addEventListener("click",function(){
-    if(get_xPosition(r)+r.offsetWidth+40 < b.offsetWidth){
+    if(r.style.left+r.offsetWidth+40 < b.offsetWidth){
       r.style.left = r.offsetLeft + 40 + "px";
       console.log(r.style.left);
     }
