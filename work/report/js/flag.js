e2ee0c507cac816084915181fc7a662849c7f6ca
YangYu8595 <yy85959948@gmail.com>
2020-03-14 23:28:51 +0100

    lab3

--- work/report/js/flag.js
+++ work/report/js/flag.js
@@ -11,7 +11,9 @@
 
     btn.onclick = function(){
       flag.style.visibility = hidden;
+      flag.style.width = 500px;
     }
+
     function drawFlagElfenbenskusten()  {
         var flagElfenbenskusten = '<div class="flag elfenbenskusten"><div class="part1"></div>' +
         '<div class="part2"></div></div>';
