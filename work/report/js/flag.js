1c67706da90727118e7216f3886aca6292a7114b
YangYu8595 <yy85959948@gmail.com>
2020-03-15 00:06:40 +0100

    lab3

--- work/report/js/flag.js
+++ work/report/js/flag.js
@@ -10,7 +10,7 @@
     var flag = document.getElementById("flag");
 
     btn.onclick = function(){
-      flag.style.visibility = hidden;
+      // flag.style.visibility = hidden;
       addClass(flag,"flag2");
        // flag.style.width = 500px;
     }
